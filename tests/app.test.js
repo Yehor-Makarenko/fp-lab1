e5994c3597d887dfe3f238fea6e06c7c726fb961
Yehor Makarenko <mak.yegor@gmail.com>
2024-10-29 20:59:33 +0200

    Fixed tests

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,9 +1,8 @@
 const request = require('supertest');
-const express = require('express');
-const app = require('./index.js'); // Імпортуємо ваш Express-додаток
+const app = require('../index.js'); // Імпортуємо ваш Express-додаток
 
 describe('GET /products/:productId', () => {
-  it('should return a product with the correct id and name', async () => {
+  test('should return a product with the correct id and name', async () => {
     const response = await request(app).get('/products/1');
     
     expect(response.status).toBe(200);
