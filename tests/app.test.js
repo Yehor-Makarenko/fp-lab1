26db6645cc537341b0a1d655fd85627b59cb5892
Yehor Makarenko <mak.yegor@gmail.com>
2024-10-29 21:26:17 +0200

    Added test report

--- tests/app.test.js
+++ tests/app.test.js
@@ -17,3 +17,14 @@ describe('GET /products/:productId', () => {
     });
   });
 });
+
+module.exports = {
+  testEnvironment: 'node',
+  reporters: [
+    'default',
+    ['jest-junit', {
+      outputDirectory: './reports/jest', // Директорія для зберігання звітів
+      outputName: 'junit.xml', // Ім'я файлу звіту
+    }],
+  ],
+};
